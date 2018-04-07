ea06e1b9fcee358981857f0768461158cfbf66ed
yuxinwen <yuxinwen@lvmama.com>
2018-04-07 22:04:12 +0800

    Initial Commit

---
+++ RunTimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunTimeDemo
+//
+//  Created by 新闻 on 2018/4/7.
+//  Copyright © 2018年 新闻. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
